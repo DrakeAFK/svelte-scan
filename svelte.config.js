4f333889e1ed4de82ca5d1cf22eb666ac12aad08
DrakeAFK <drakehopkins@outlook.com>
2024-05-14 13:11:47 -0400

    cloudflare setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter()
-  },
+
+export default {
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+	  platformProxy: {
+		persist: './your-custom-path'
+	  }
+		})
+	},
   preprocess: vitePreprocess()
 };
-export default config;
